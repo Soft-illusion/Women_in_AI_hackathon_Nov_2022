73fbc38a78396413a5be805f05436bd67813a6c4
Koyal Bhartia <kbhartia@innovative-logistics.com>
2022-11-03 21:57:09 -0500

    World axis direction fixed + Controller code comments

--- mockathon_question_2/controllers/ground/ground.py
+++ mockathon_question_2/controllers/ground/ground.py
@@ -1,59 +1,62 @@
+
+#Webots import statments
 from controller import Display
 from controller import Supervisor
 from controller import Keyboard
 
 TIME_STEP = 64
 
-# size of the ground in meters.
+# size of full ground in meters.
 GROUND_X = 9.9
 GROUND_Y = 9.9
 
-super = Supervisor() # Supervisor node used to fetch location of moving robot. 
+# Supervisor node used to fetch location of moving robot. 
+super = Supervisor() 
 
-# Enable Keyboard to use for teleoping the robot
+# Enable Keyboard to allow robot to draw / undraw based on robot keys 
 keyboard=Keyboard()
-keyboard.enable(TIME_STEP) # Enabled keyboard to provide feedback with time difference equat to timestep.
+keyboard.enable(TIME_STEP) # Keyboard will provide feedback with time difference equal to timestep.
 
-# First we get a handler to devices
+# Handler to canvas backgroud device 
 display = super.getDevice("ground_display")
 
-# get the properties of the Display
+# Fetch properties of above Display
 width = display.getWidth()
 height = display.getHeight()
 
-# getting access to robot handle to access its tree.
+# Get access to robot handle to access its tree.
 mybot = super.getFromDef("diff_robot")
 
-# getting translation field of robot to make changes on the ground.
+# Get translation field to now robot's current location. 
 translationField = mybot.getField("translation")
 
-# Loading background from an image. 
+# Load Curve background from an image. 
 background = display.imageLoad("../../worlds/textures/sin.png")
-display.imagePaste( background, 0, 0) # adding background to the display node to make changes on the background.
+display.imagePaste( background, 0, 0) # Adding background to the display node to make changes on the background.
 
 # // set the pen to remove the texture
 display.setAlpha( 0.0)
 
-draw = True # Flag to to enable and disable drawing.
+draw = True # Flag to enable and disable drawing.
 
 while super.step(TIME_STEP) != -1:
 
-  # getting currently pressed key on the keyboard.    
+  # get currently pressed key on the keyboard.    
   key=keyboard.getKey()
-  print(key , "   ====  Key")
-  if (key==80): # If 'p' press enable drawing
+  #print("Key pressed: ", key)
+  if (key==80): # P press returns 80 on key variable 
     print ('P is pressed')
-    draw=True
-  elif(key==79): # If 'o' press disable drawing
+    draw=True #If 'P' press enable drawing
+  elif(key==79): # O press returns 79 on key variable
     print ('O is pressed')
-    draw=False
+    draw=False #If 'O' press enable drawing
 
-  if draw:  # Condition to draw
-    translation = translationField.getSFVec3f()
-    x = int(height * (translation[1] + GROUND_X / 2) / GROUND_X) # Mapping robot translation in x axis to pixel value
-    y = int(width * (translation[0] + GROUND_Y / 2) / GROUND_Y) # Mapping robot translation in y axis to pixel value
+  if draw:  # Enable robot coloring ONLY if initially no key is pressed or P is pressed ; stop colouring if O is pressed.
+    translation = translationField.getSFVec3f() #Fetch robot location to color at that location
+    y = int(height * (translation[1] + GROUND_X / 2) / GROUND_X) # Mapping robot translation in x axis to pixel value
+    x = int(width * (translation[0] + GROUND_Y / 2) / GROUND_Y) # Mapping robot translation in y axis to pixel value
     
-    # Remove 10 x 10 pixel at location x,y in the display image plane to see base Women in AI and Robotics logo.
+    # Remove 10 x 10 pixel at location x,y in the display image plane to see the base "Women in AI and Robotics" logo.
     display.fillOval( x, y, 10, 10)
 
   pass
